d9651369bc072e9883b57c35b5440b223cbd1889
AgonIdrizi <agon.88@hotmail.com>
2018-09-16 13:20:13 +0200

    gemfile changed

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -6,7 +6,7 @@ class FlightsController < ApplicationController
 	      redirect_to root_path
 	    else
 	      @flights = Flight.search(params)
-	      flash.now[:danger] = 'Nothing found' if @flights.length == 0
+	      flash.now[:danger] = 'Nothing found' if @flights.length == 0 && !params[:commit].to_s.blank?
 	 	  flash.now[:danger] = 'Airports codes must be different' if Flight.check_for_same_airport_values(params)
 	 	  	
 		  render 'index'
